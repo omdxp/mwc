e2c4aaef0251e96f67a5e55a972b86a6e698258d
omdxp <omarbelghaouti@gmail.com>
2022-09-15 17:58:30 +0100

    take input from user and display the weight

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    println!("Enter your weight (kg):");
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)?;
+    let weight = buf.trim().parse::<f32>().unwrap();
+    let weight_on_mars = calculate_weight_on_mars(weight);
+    println!("Weight on Mars: {} kg", weight_on_mars);
+    Ok(())
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
